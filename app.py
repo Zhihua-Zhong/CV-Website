bc48cefbba1d73153a024f4e99e7093af8502b28
Zhihua Zhong <70009282+Zhihua-Zhong@users.noreply.github.com>
2023-11-11 19:19:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,16 @@ st.subheader("Academic Article Publication History")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
+    
+st.write('\n')
+st.subheader("Presentation on Academic Conference")
+st.write("---")
+st.write(
+"""
+- [2] Conference: Physical Society of Japan (日本物理学会); Title: 'Electric circuit network description of urban human mobility based on GPS data'; Date: 2023/8
+- [1] Conference(International): Statphys28; Title: 'An analysis of human flow pattern based on Revised Electric Circuit Model'; Date: 2023/9
+"""
+)
 
 
 # --- SKILLS ---
@@ -105,20 +115,20 @@ st.write('\n')
 st.subheader("Experience History")
 st.write("---")
 
-# --- JOB 1
+# --- doctoral education
 st.write("🏆", "**Doctoral education at Tokyo Institute of Technology**")
 st.write("09/2022 - Present")
 st.write(
 """
 - [Internship, 2023/11-present] 5. Internship of company Sony: Develop a website to interact with and present the result of the data analysis.
 - [Joint Research, 2023/4-present] 4. Joint Research with company Sony: Analyse the sales data of Sony’s products, such as TVs and cameras, and predict the units and price of sales to solve the supply chain problem.
-- [Presentation, 2023/9] 3. Gave a presentation to Physical Society of Japan (日本物理学会) about 'Electric circuit network description of urban human mobility based on GPS data.'
-- [Presentation, 2023/8] 2. Gave a presentation to Statphys28 (International academic conference) about 'An analysis of human flow pattern based on Revised Electric Circuit Model.'
+- [Presentation, 2023/9] 3. Presentation to Physical Society of Japan (日本物理学会) about 'Electric circuit network description of urban human mobility based on GPS data.'
+- [Presentation, 2023/8] 2. Presentation to Statphys28 (International academic conference) about 'An analysis of human flow pattern based on Revised Electric Circuit Model.'
 - [GPA] 1. Major GPA so far: 3.8/4.5.
 """
 )
 
-# --- JOB 2
+# --- master's education
 st.write('\n')
 st.write("🏆", "**Master's education at Jinan University**")
 st.write("09/2019 - 07/2022")
